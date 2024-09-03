5a587f2d21fa97558fdea6c5a466ab153d562860
Benan Redzhebov <benanredzhebov@gmail.com>
2024-09-03 14:27:30 +0200

    main function ready

--- CPP_00/ex01/src/main.cpp
+++ CPP_00/ex01/src/main.cpp
@@ -6,8 +6,43 @@
 /*   By: benanredzhebov <benanredzhebov@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 21:25:50 by benanredzhe       #+#    #+#             */
-/*   Updated: 2024/09/02 21:26:29 by benanredzhe      ###   ########.fr       */
+/*   Updated: 2024/09/03 14:17:37 by benanredzhe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/PhoneBook.hpp"
+
+int	main()
+{
+	Phonebook	phonebook;
+	std::string	command;
+
+	phonebook.showInstruction();
+	while (true)
+	{
+		std::cout << "Enter command: ";
+		if (std::cin.eof() == true)
+		{
+			std::cout << "" << std::endl;
+			std::cout << "Exiting phonebook... GOOD BYE" << std::endl;
+			break ;
+		}
+		std::getline(std::cin, command);
+		if (command == "EXIT")
+		{
+			std::cout << "Exiting phonebook... GOOD BYE" << std::endl;
+			break ;
+		}
+		else if (command == "ADD")
+		{
+			if (!phonebook.getInfo())
+				break ;
+		}
+		else if (command == "SEARCH")
+		{
+			if (!phonebook.searchEntries())
+				break;
+		}
+	}
+	return (0);
+}
