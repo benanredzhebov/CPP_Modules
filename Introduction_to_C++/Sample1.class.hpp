9210997fb4b2542437dee58ac938cb919b334341
Benan Redzhebov <benanredzhebov@gmail.com>
2024-09-07 07:17:12 +0200

    unstaget changes

---
+++ Introduction_to_C++/Sample1.class.hpp
@@ -0,0 +1,13 @@
+#ifndef SAMPLE1_CLASS_H
+# define SAMPLE1_CLASS_H
+
+class Sample1 {
+public:
+	char	a1; //attribute
+	int	a2; //attribute
+	float	a3; //attribute
+
+	Sample1(char p1, int p2, float p3); //parameters inside of brackets
+	~Sample1(void);
+};
+#endif	
