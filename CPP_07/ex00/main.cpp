ca03244d53f0fcb7f20522a356b2050287ad68dc
benanredzhebov <benanredzhebov@gmail.com>
2024-10-28 15:06:54 +0100

    started CPP_07

---
+++ CPP_07/ex00/main.cpp
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beredzhe <beredzhe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 13:56:01 by beredzhe          #+#    #+#             */
+/*   Updated: 2024/10/28 14:46:15 by beredzhe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "./whatever.hpp"
+
+// #define MAX_VAL 750
+
+// int main(int, char**)
+// {
+// 	Array<int> numbers(MAX_VAL);
+// 	int* mirror = new int[MAX_VAL];
+// 	srand(time(NULL));
+// 	for (int i = 0; i < MAX_VAL; i++)
+// 	{
+// 		const int value = rand();
+// 		numbers[i] = value;
+// 		mirror[i] = value;
+// 	}
+// 	//SCOPE
+// 	{
+// 		Array<int> tmp = numbers;
+// 		Array<int> test(tmp);
+// 	}
+
+// 	for (int i = 0; i < MAX_VAL; i++)
+// 	{
+// 		if (mirror[i] != numbers[i])
+// 		{
+// 			std::cerr << "didn't save the same value!!" << std::endl;
+// 			return 1;
+// 		}
+// 	}
+// 	try
+// 	{
+// 		numbers[-2] = 0;
+// 	}
+// 	catch(const std::exception& e)
+// 	{
+// 		std::cerr << e.what() << '\n';
+// 	}
+// 	try
+// 	{
+// 		numbers[MAX_VAL] = 0;
+// 	}
+// 	catch(const std::exception& e)
+// 	{
+// 		std::cerr << e.what() << '\n';
+// 	}
+
+// 	for (int i = 0; i < MAX_VAL; i++)
+// 	{
+// 		numbers[i] = rand();
+// 	}
+// 	delete [] mirror;//
+// 	return 0;
+// }
+
+int main(void)
+{
+	int a = 3;
+	int b = 2;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	return 0;
+}
---
+++ CPP_07/ex00/whatever.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beredzhe <beredzhe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 14:10:41 by beredzhe          #+#    #+#             */
+/*   Updated: 2024/10/28 14:43:50 by beredzhe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+
+void	swap(T &a, T &b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T min(T &a, const T &b) {
+	return (a < b) ? a : b; // if a is less than b return a.
+}
+
+template <typename T>
+T max(T &a, const T &b) {
+	return (a > b) ? a : b;
+}
+
+#endif
+
+
+
+/*Templates allow us to write generic code that can work with different
+types without specyfing the exact data type in advance.
+We can use "T" as a placeholder for any type.*/
